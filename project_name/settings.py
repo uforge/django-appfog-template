9138997be2c7900511f63cb1a627d139e5ac9a4d
Vash <vshjxyz@gmail.com>
2013-03-08 12:11:37 +0100

    refs #290 adding the SECRET_KEY parameter (needed in Django >= 1.4)

--- project_name/settings.py
+++ project_name/settings.py
@@ -69,6 +69,9 @@ USE_L10N = True
 # If you set this to False, Django will not use timezone-aware datetimes.
 USE_TZ = True
 
+# Secret key needed by django >= 1.4
+SECRET_KEY = '{{ secret_key }}'
+
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
 MEDIA_ROOT = os.path.join(PUBLIC_DIR, 'media')
